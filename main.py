7407f69820c249237eea604919f8fd84182ef3f3
Aleksej Demidenko00 <78086659+aleksej00@users.noreply.github.com>
2022-01-21 07:32:33 +0000

    all

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-text = input("введите текст: ")
-print(text[::-1])
+text = input("ievadit tekstu: ")
+def replaceO (text):
+  if text.count("o")>0 or text.count("o")>0:
+    text = text.replace("o","%")
+    text = text.replace("o","%")
+    print(text)
+  else:
+    text = "nav burtu O vai o."
+    print(text)
+  return text
+replaceO (text)
